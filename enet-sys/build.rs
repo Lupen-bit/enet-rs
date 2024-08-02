967c1c2e4143ff7608ee4330482e897edb072056
Cendy <cendyemail@gmail.com>
2024-08-02 09:37:38 +0700

    fix: could not find native static library

--- enet-sys/build.rs
+++ enet-sys/build.rs
@@ -7,6 +7,7 @@ use std::path::PathBuf;
 
 fn main() {
     let target = env::var("TARGET").unwrap();
+    let is_debug = env::var("DEBUG").unwrap() == "true";
     let bindings = bindgen::Builder::default()
         .clang_arg("-Ivendor/enet/include/")
         .header("wrapper.h")
@@ -27,12 +28,20 @@ fn main() {
     eprintln!("LUL: {}", dst.display());
 
     if target.contains("windows") {
+        if is_debug {
+            println!(
+                "cargo:rustc-link-search=native={}/build/Debug",
+                dst.display()
+            );
+        } else {
+            println!(
+                "cargo:rustc-link-search=native={}/build/Release",
+                dst.display()
+            );
+        }
         println!("cargo:rustc-link-lib=dylib=winmm");
+    } else {
+        println!("cargo:rustc-link-search=native={}/build", dst.display());
     }
-
-    println!(
-        "cargo:rustc-link-search=native={}/lib/static",
-        dst.display()
-    );
     println!("cargo:rustc-link-lib=static=enet");
 }
